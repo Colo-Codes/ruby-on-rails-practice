6ac2c9ad8b0917e66208475e0f74bc21bb291830
Damian Demasi <damian.demasi.1@gmail.com>
2021-10-20 07:35:04 +1030

    Initial commit

---
+++ area_code_dictionary.rb
@@ -0,0 +1,27 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
+# Get city names from the hash
+def get_city_names(somehash)
+# Write code here
+end
+ 
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+# Write code here
+end
+ 
+# Execution flow
+loop do
+# Write your program execution code here
+end
