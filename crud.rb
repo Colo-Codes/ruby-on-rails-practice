0a96e0d331760a16ebf826e8828662eaba26b6d5
Damian Demasi <damian.demasi.1@gmail.com>
2021-10-30 12:23:59 +1030

    Module creation

--- crud.rb
+++ crud.rb
@@ -1,44 +1,32 @@
-require 'bcrypt'
-
-users = [
-    { username: "mashrur", password: "password1"},
-    { username: "jack", password: "password2"},
-    { username: "arya", password: "password3"},
-    { username: "jonshow", password: "password4"},
-    { username: "heisenberg", password: "password5"},
-    { username: "damian", password: "password6"}
-]
-
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_passwords(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+module Crud
+    
+  require 'bcrypt'
+  
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-  list_of_users
-end
-
-puts create_secure_passwords(users)
-
-def authenticate_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
-      return user_record
+  
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+  
+  def create_secure_passwords(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
     end
+    list_of_users
   end
-  "Credentials were not correct"
+  
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials were not correct"
+  end
+  
 end
-
-20.times {print "-"}
-puts
-puts authenticate_user("damian", "password6", users)
-puts authenticate_user("damian", "wrongpass", users)
 
 #=> {:username=>"mashrur", :password=>"$2a$12$BDjTdUozMzmEL5IrnyFbqO1YM7iwbioqUBn/vfYMKRkA/2WDsuZmu"}
 #=> {:username=>"jack", :password=>"$2a$12$2P590gB9hXMterswF7IjzOrIiGm3ycKwj2KTnj7ZOMdh5nyCCGM5S"}
